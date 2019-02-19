5171f9c78659b77ec09e80b9de41aba602e22a0a
Rodrigo Sánchez <rodrigosanchez@ciencias.unam.mx>
2019-02-19 01:01:42 -0600

    Estructura al repositorio.

--- Ejercicios/Ejercicio02/src/vigenere.py
+++ Ejercicios/Ejercicio02/src/vigenere.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from collections import Counter
+from collections import Counter, OrderedDict
 import sys
 
 # alphabet = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
@@ -8,14 +8,14 @@ import sys
 # Calcula el porcentaje de frecuencias que aparece el caracter especificado.
 def frequency(text):
     length = len(text)
-    counter = Counter(list(text))
-    freqList = {}
+    counter = OrderedDict(Counter(list(text)).most_common())
+    freqList = []
     for char in counter:
         average = counter[char]/length
-        freqList[char] = average
+        freqList.append(average)
         # Imprimimos el porcentaje que aparece cada letra.
         # Hacemos un cast de average de float a string.
-        print('El porcentaje que aparece la '+char+' es '+repr(average*100)+'%')
+        # print('El porcentaje que aparece la '+char+' es '+repr(average*100)+'%')
     # print(freqList)
     return freqList
 
@@ -26,15 +26,14 @@ def block(t, message):
         print('Debes introducir un número mayor a 0.');
     else:
         longitud = len(message)
-        print(message[0:longitud:t])
+        # print(message[0:longitud:t])
         return message[0:longitud:t]
 
 # Función que calcula la I 
 def I(list):
     I = 0
-    for i in list.keys():
-        I += list[i] ** 2
-        # print("Got key", i, "which maps to value", freqList[i])
+    for i in list:
+        I += i**2
     # print(I)
     return I
 
@@ -47,7 +46,7 @@ with open(sys.argv[1], 'r') as f:
 
 # print(cypherClean[:49])
 
-print(frequency(cypherClean))
+# print(frequency(cypherClean))
 
 # block(0, cypherClean) # Caso de error.
 # block(1, cypherClean) # Devuelve la misma entrada.
@@ -57,26 +56,36 @@ print(frequency(cypherClean))
 # frequency(block(5, cypherClean))
 
 for n in range(1, len(cypherClean)):
+    print(frequency(block(n, cypherClean)))
     if(I(frequency(block(n, cypherClean))) >= 0.0741):
+        q = frequency(block(n, cypherClean))
+        print(q)
         break
 
-letterFreq = {'E':13.68,'A':12.53,'O':8.68,'S':7.98,'R':6.87,'N':6.71,'I':6.25,'D':5.86,'L':4.97,
+letterFreq = {'E':13,'A':11.1,'O':9.7,'S':7.98,'R':6.87,'N':6.71,'I':6.25,'D':5.86,'L':4.97,
               'C':4.68,'T':4.63,'U':3.93,'M':3.15,'P':2.51,'B':1.42,'G':1.01,'V':0.9,'Y':0.9,
               'Q':0.88,'H':0.7,'F':0.69,'Z':0.52,'J':0.44,'Ñ':0.31,'X':0.22,'K':0.02,'W':0.01}
-letterFreq2 = {0:13.68,1:12.53,2:8.68,3:7.98,4:6.87,5:6.71,6:6.25,7:5.86,8:4.97,
-              9:4.68,10:4.63,11:3.93,12:3.15,13:2.51,14:1.42,15:1.01,16:0.9,17:0.9,
-              18:0.88,19:0.7,20:0.69,21:0.52,22:0.44,23:0.31,24:0.22,25:0.02,26:0.01}
+p = {0:13,1:11.1,2:9.7,3:8.2,4:8,5:7.7,6:6.9,7:5.3,8:5.2,
+     9:4.5,10:3.6,11:3.6,12:3,13:2.9,14:1.4,15:1.3,16:1,17:0.8,
+     18:0.7,19:0.6,20:0.6,21:0.3,22:0.3,23:0.2,24:0.1,25:0,26:0}
 # letterFrequency = ['E','A','O','S','R','N','I','D','L','C','T','U','M','P','B','G','V','Y','Q','H','F','Z','J','Ñ','X','K','W']
 
-# qi = 
+# print(q)
 
 Ik = 0
-pi = 0
-for k in range(27):
-    for i in range(1, 27):
-        pi = letterFreq2[i]
-    for i in range(1, 28):
-        Ik = pi * 1
-    if(Ik >= 0.0741):
-        break
-        # print(letterFreq2[n])
+for k in range(26):
+    for i in range(26):
+        # print("i:" + repr(i) +" k:" + repr(k))
+        # print(q[(i+k)%23])
+        Ik += (p[i] * q[(i+k)% 25])
+        if(Ik >= 0.0741):
+            print('hola: '+repr(Ik))
+            Ik =0
+            break        
+
+print(Ik)
+    # for i in range(1, 28):
+    #     Ik = pi * 1
+    # if(Ik >= 0.0741):
+    #     break
+        # print(p[n])
